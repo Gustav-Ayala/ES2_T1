8451f360c86d77eddc04c7b9d9f83e46c1584a1e
Coconutoos <gteixeiraayala@gmail.com>
2024-03-07 20:55:38 -0300

    get Especialidade servico

---
+++ src/main/java/com/es2/t1/controllers/EspecialidadeController.java
@@ -0,0 +1,23 @@
+package com.es2.t1.controllers;
+
+import com.es2.t1.domain.EspecialidadeEntity;
+import com.es2.t1.services.EspecialidadeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/Especialidade")
+public class EspecialidadeController {
+    private final EspecialidadeService especialidadeService;
+
+    @GetMapping
+    public ResponseEntity<?> readEspecialidade(){
+        return especialidadeService.readEspecialidades();
+    }
+}
